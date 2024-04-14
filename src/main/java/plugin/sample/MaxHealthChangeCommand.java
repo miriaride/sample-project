bec33c6c52bd8f28362983c1294636cda4c8755f
miriaride <miriaride@users.noreply.github.com>
2024-04-14 21:23:45 +0900

    MaxHealthChangeCommand.javaの重複していたコードの削除

--- src/main/java/plugin/sample/MaxHealthChangeCommand.java
+++ src/main/java/plugin/sample/MaxHealthChangeCommand.java
@@ -12,13 +12,6 @@ public class MaxHealthChangeCommand implements CommandExecutor {
   // プレイヤーの最大体力を変更するメソッド
   double newHealth = 40.0;
 
-  public void changeMaxHealth(Player player, double newHealth) {
-    AttributeInstance maxHealthAttribute = player.getAttribute(Attribute.GENERIC_MAX_HEALTH);
-    if (maxHealthAttribute != null) {
-      maxHealthAttribute.setBaseValue(newHealth);
-    }
-  }
-
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if (sender instanceof Player player) {
@@ -26,7 +19,7 @@ public class MaxHealthChangeCommand implements CommandExecutor {
       if (maxHealthAttribute != null) {
         maxHealthAttribute.setBaseValue(newHealth);
       }
-      player.setHealth(40);
+      player.setHealth(newHealth);
     }
     return false;
   }

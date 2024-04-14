3e6a7bb18878a88e88c66345bc9930cab8fad986
miriaride <miriaride@users.noreply.github.com>
2024-04-14 22:51:27 +0900

    setHealthを固定値→コマンド引数で受け取るように変更

--- src/main/java/plugin/sample/MaxHealthChangeCommand.java
+++ src/main/java/plugin/sample/MaxHealthChangeCommand.java
@@ -10,16 +10,21 @@ import org.bukkit.entity.Player;
 public class MaxHealthChangeCommand implements CommandExecutor {
 
   // プレイヤーの最大体力を変更するメソッド
-  double newHealth = 40.0;
+
 
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if (sender instanceof Player player) {
       AttributeInstance maxHealthAttribute = player.getAttribute(Attribute.GENERIC_MAX_HEALTH);
-      if (maxHealthAttribute != null) {
-        maxHealthAttribute.setBaseValue(newHealth);
+      if (args.length == 1) {
+        double newHealth = Double.parseDouble(args[0]);
+        if (maxHealthAttribute != null) {
+          maxHealthAttribute.setBaseValue(newHealth);
+        }
+        player.setHealth(newHealth);
+      } else {
+        player.sendMessage("Nooooooooooooooooo!!");
       }
-      player.setHealth(newHealth);
     }
     return false;
   }
